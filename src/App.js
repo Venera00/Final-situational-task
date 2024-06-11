19a6b19dfca13afe5a7b06a81b4bd514024bb5a6
Venera <veneraomurzakkyzy@gmail.com>
2024-06-11 12:51:43 +0600

    Add registration page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
 import "./Register.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div>
+      <h3 className="register__title">Register</h3>
+      <form onSubmit={formik.handleSubmit} className="form">
+        <div className="form__component">
+          <label htmlFor="email">Name:</label>
+          <input
+            id="name"
+            name="name"
+            type="name"
+            placeholder="Veronika"
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="form__component">
+          <label htmlFor="email">Email Address:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="example@gmail.com"
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="form__component">
+          <label htmlFor="email">Password:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Write password"
+            onChange={formik.handleChange}
+          />
+        </div>
+        <button className="register__btn">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
